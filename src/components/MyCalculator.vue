00cf9df24a35042d4d7a8c6eb373e93796d2d24e
Sondre Malerud <sondrmal@stud.ntnu.no>
2023-01-29 13:02:48 +0100

    Better mobile support

--- src/components/MyCalculator.vue
+++ src/components/MyCalculator.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="calculator">
-      <div class="controls">
-      <div class="display">{{ number || '0'}}</div>
+    <div class="controls">
+      <div class="display">{{ number || "0" }}</div>
       <button class="calculator-button" @click="clear">C</button>
       <button class="calculator-button" @click="getAns">ANS</button>
       <button class="calculator-button" @click="deleteDigit">DEL</button>
@@ -18,11 +18,12 @@
       <button class="calculator-button" @click="addDigit('2')">2</button>
       <button class="calculator-button" @click="addDigit('3')">3</button>
       <button class="calculator-button" @click="addOperator('+')">+</button>
-      <button class="calculator-button" id="zero" @click="addDigit('0')">0</button>
+      <button class="calculator-button" id="zero" @click="addDigit('0')">
+        0
+      </button>
       <button class="calculator-button" @click="addComma">.</button>
       <button class="calculator-button" @click="evaluate">=</button>
     </div>
-
 
     <div class="log">
       <ul>
@@ -32,27 +33,25 @@
       </ul>
     </div>
   </div>
-
-
-  
 </template>
 
 <script>
 export default {
+  name: "MyCalculator",
   data() {
     return {
-      number: '',
-      ans: '',
-      currentOperator: '',
-      previousNumber: '0',
-      logItems: []
-    }
+      number: "",
+      ans: "",
+      currentOperator: "",
+      previousNumber: "0",
+      logItems: [],
+    };
   },
   methods: {
     clear() {
-      this.number = '';
-      this.previousNumber = '';
-      this.currentOperator = '';
+      this.number = "";
+      this.previousNumber = "";
+      this.currentOperator = "";
     },
 
     addDigit(num) {
@@ -60,9 +59,9 @@ export default {
     },
 
     addComma() {
-      if (!this.number.includes('.')){
-        if (this.number === '' || this.number === '0') this.number = '0.'
-        else this.number = this.number + '.';
+      if (!this.number.includes(".")) {
+        if (this.number === "" || this.number === "0") this.number = "0.";
+        else this.number = this.number + ".";
       }
     },
 
@@ -70,51 +69,54 @@ export default {
       this.number = this.number.toString().slice(0, -1);
     },
 
-    getAns(){
+    getAns() {
       this.number = this.ans;
     },
 
     addOperator(operator) {
       this.operator = operator;
       this.previousNumber = this.number;
-      this.number = '';
-
+      this.number = "";
     },
 
     evaluate() {
       const temp = this.number;
       this.number = eval(this.previousNumber + this.operator + this.number);
 
-      this.logItems.unshift(this.previousNumber + ' ' + this.operator + ' ' + temp + ' = ' + this.number);
+      this.logItems.unshift(
+        this.previousNumber +
+          " " +
+          this.operator +
+          " " +
+          temp +
+          " = " +
+          this.number
+      );
       this.ans = this.number;
-      this.previousNumber = temp;      
-    }
-  }
-}
+      this.previousNumber = temp;
+    },
+  },
+};
 </script>
 
 <style scoped>
-.controls{
-
+.controls {
   max-width: 400px;
   min-width: 50%;
   font-size: 20px;
   display: grid;
   grid-template-columns: repeat(4, 1fr);
   grid-auto-rows: minmax(50px, auto);
-
 }
 
 .calculator {
-  display:flex;
+  display: flex;
   flex-direction: row;
   margin: 0 auto;
   justify-content: center;
   max-width: 800px;
 }
 
-
- 
 .display {
   text-align: right;
   padding-right: 2%;
@@ -122,7 +124,6 @@ export default {
   background-color: rgb(97, 97, 200);
   background-color: #202340;
   font-size: 40px;
-
 }
 
 h1 {
@@ -130,38 +131,35 @@ h1 {
 }
 
 div {
-  color:white;
+  color: white;
 }
 
 .log {
   width: 200px;
   text-align: left;
   background-color: #202340;
-  overflow:scroll;
+  overflow: scroll;
   overflow-x: hidden;
   overflow-y: auto;
 
   align-self: flex-start;
   height: 305px;
-
-  
 }
 
 .calculator-button {
-  border:1px #363959 solid;
+  border: 1px #363959 solid;
   background-color: #0f1126;
-  color:white;
+  color: white;
   font-size: 30px;
-
 }
 
-.calculator-button:hover  {
-  border:1px #999 solid;
-
+.calculator-button:hover {
+  border: 1px #999 solid;
 }
 
 .calculator-button:active {
-  background-color: #5922a5;
+  /*   background-color: #5922a5;*/
+  background-color: #42b983;
 }
 
 #zero {
@@ -177,25 +175,28 @@ div {
   padding-left: 2%;
 }
 
-
 @media only screen and (max-width: 768px) {
   .calculator {
     flex-direction: column;
-    width:100%;
 
     justify-content: center;
+    align-items: center;
+  }
+
+  .controls {
+    width: 100%;
+    min-height: 200px;
+    margin: 0 auto;
   }
   .log {
-    width:400px;
+    width: 100%;
+    max-width: 400px;
     min-height: 200px;
-    margin: 0;
-
+    margin: 0 auto;
   }
 
   .log ul {
-    padding-top:0%;
+    padding-top: 0%;
   }
 }
-
-
 </style>
