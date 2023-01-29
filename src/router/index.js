51d092b1003f29ed05800115ed0dfc5f4d16ffdc
Sondre Malerud <sondrmal@stud.ntnu.no>
2023-01-29 16:25:59 +0100

    Small fix

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from "vue-router";
 import Home from "../views/Home.vue";
+import ContactForms from "../views/ContactForms.vue";
 
 const routes = [
   {
@@ -13,8 +14,7 @@ const routes = [
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/ContactForms.vue")
+    component: ContactForms
   }
 ];
 
