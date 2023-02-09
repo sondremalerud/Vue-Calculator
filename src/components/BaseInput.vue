53bc4012b42b1c56e2fddc1c8bfe4a1d77723539
Sondre Malerud <sondrmal@stud.ntnu.no>
2023-02-09 22:12:11 +0100

    Ting og tang

---
+++ src/components/BaseInput.vue
@@ -0,0 +1,27 @@
+<template>
+    <label v-if="label">{{ label }}</label>
+    <input
+      v-bind="$attrs"
+      :value="modelValue"
+      :placeholder="label"
+      @input="$emit('update:modelValue', $event.target.value)"
+      class="field"
+    >
+  </template>
+  
+  <script>
+    
+    export default {
+        name: "BaseInput",
+        props: {
+            label: {
+                type: String,
+                default: ''
+            },
+            modelValue: {
+                type: [String, Number],
+                default: ''
+            }
+        }
+    }
+  </script>
--- src/views/ContactForms.vue
+++ src/views/ContactForms.vue
@@ -1,5 +1,35 @@
 <template>
-    <div class="contact-forms">
-        <h1>Hello world</h1>
-    </div>
+    <h1>Kebab</h1>
+    <form>
+        <p>keab</p>
+        <BaseInput
+            v-model="form.formName"
+            label="Name"
+            type="text"
+        />
+
+    </form>
+
+
 </template>
+
+<script>
+import BaseInput from "@/components/BaseInput.vue";
+
+
+export default {
+    name: "ContactForms",
+    data() {
+        return {
+            form: {
+                formName: "",
+                email: "",
+                message: ""
+            }
+        }
+    },
+    components: {
+        BaseInput
+    }
+}
+</script>
