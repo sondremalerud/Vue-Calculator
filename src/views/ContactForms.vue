077981cb207deb1cf2973027b90e6ac310af0d42
Sondre Malerud <sondre_mal@hotmail.no>
2023-02-10 11:08:59 +0100

    123

--- src/views/ContactForms.vue
+++ src/views/ContactForms.vue
@@ -1,14 +1,38 @@
 <template>
-    <h1>Kebab</h1>
-    <form>
-        <p>keab</p>
-        <BaseInput
-            v-model="form.formName"
-            label="Name"
-            type="text"
-        />
+    <div class="contact-form">
 
-    </form>
+    
+        <h1>123 🧌</h1>
+        
+            
+            <BaseInput
+                v-model="form.formName"
+                label="Name"
+                type="text"
+            />
+
+            <BaseInput
+                v-model="form.email"
+                label="Email"
+                type="text"
+            />
+
+            <BaseInput
+                v-model="form.message"
+                label="Message"
+                type="text"
+                id="message"
+            />
+        
+            <textarea name="Message" id="message" cols="30" rows="10"></textarea>
+
+            <button>Submit</button>
+
+        
+
+    </div>
+
+    
 
 
 </template>
@@ -33,3 +57,30 @@ export default {
     }
 }
 </script>
+
+<style scoped>
+
+
+    div {
+        color: white;
+    }
+
+    .contact-form {
+        display: flex;
+        max-width: 500px;
+        flex-direction: column;
+        max-width: 400px;
+        min-width: 50%;
+        font-size: 20px;
+        align-items: center;
+        margin: 0 auto;
+
+    }
+
+    #message {
+        resize: both;
+
+    }
+
+
+</style>
