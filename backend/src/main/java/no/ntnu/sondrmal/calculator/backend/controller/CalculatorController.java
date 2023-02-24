aff158981c8e8267472d51db94e5b111d9434851
Sondre Malerud <sondrmal@stud.ntnu.no>
2023-02-24 12:43:29 +0100

    ✅ Øving 4 godkjent

--- backend/src/main/java/no/ntnu/sondrmal/calculator/backend/controller/CalculatorController.java
+++ backend/src/main/java/no/ntnu/sondrmal/calculator/backend/controller/CalculatorController.java
@@ -1,6 +1,9 @@
 package no.ntnu.sondrmal.calculator.backend.controller;
 
 import no.ntnu.sondrmal.calculator.backend.service.CalculatorService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -9,23 +12,15 @@ import org.springframework.web.bind.annotation.*;
 @CrossOrigin(origins={"*"})
 public class CalculatorController {
 
+    @Autowired
+    private CalculatorService service;
+    Logger logger = LoggerFactory.getLogger(CalculatorController.class);
 
     @PostMapping("/calculations")
     public ResponseEntity calculateExpresssion(@RequestParam("expression") String expression) {
-
-        String answer = CalculatorService.evaluate(expression);
-        System.out.println(expression);
-        System.out.println(CalculatorService.evaluate(expression));
+        logger.info("Expression received: " + expression);
+        String answer = this.service.evaluate(expression);
+        logger.info("Answer: " + answer);
         return new ResponseEntity(answer, HttpStatus.OK);
     }
-
-    @GetMapping("/kebab")
-    public void kebab() {
-
-
-        System.out.println("kebab");
-
-    }
-
-
 }
--- backend/src/main/java/no/ntnu/sondrmal/calculator/backend/service/CalculatorService.java
+++ backend/src/main/java/no/ntnu/sondrmal/calculator/backend/service/CalculatorService.java
@@ -1,10 +1,12 @@
 package no.ntnu.sondrmal.calculator.backend.service;
 
 import net.objecthunter.exp4j.ExpressionBuilder;
+import org.springframework.stereotype.Service;
 
+@Service
 public class CalculatorService {
     
-    public static String evaluate(String expression) {
+    public String evaluate(String expression) {
         ExpressionBuilder eb = new ExpressionBuilder(expression);
         return ""+eb.build().evaluate();
     }
